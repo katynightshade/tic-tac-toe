6cd8eb6ced59995bc1b2fce00a047c8ff3c73b3d
katynightshade <98486819+katynightshade@users.noreply.github.com>
2022-05-10 12:27:09 -0600

    players constructor and gameboard module

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const MakeGameboard = (() => {
+const Player = (sign) => {
+    this.sign = sign;
 
-})();
+    const getSign = () => {
+        return sign;
+    };
 
-const makePlayers = (name) => {
-
+    return { getSign };
 }
+
+const makeGameboard = (() => {
+    let pieceArray = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+
+    const setBoard = (index, sign) => {
+        pieceArray[index] = sign;
+    };
+
+    const getBoard = (index) => {
+        return pieceArray[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < pieceArray.length; i++) {
+            pieceArray[i] = '';
+        };
+    };
+
+    return {
+        setBoard,
+        getBoard,
+        reset,
+    }
+})();
 
 const gameFlow = () => {
 
