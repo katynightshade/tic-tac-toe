515d7b4cf54e5f1c3a3cfc4f1d9f7368d2a3c685
katynightshade <98486819+katynightshade@users.noreply.github.com>
2022-05-10 12:31:15 -0600

    returned to general outline js

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
-const Player = (sign) => {
-    this.sign = sign;
-
-    const getSign = () => {
-        return sign;
-    };
-
-    return { getSign };
-}
-
 const makeGameboard = (() => {
-    let pieceArray = [
-        '', '', '',
-        '', '', '',
-        '', '', ''
-    ];
-
-    const setBoard = (index, sign) => {
-        pieceArray[index] = sign;
-    };
-
-    const getBoard = (index) => {
-        return pieceArray[index];
-    };
-
-    const reset = () => {
-        for (let i = 0; i < pieceArray.length; i++) {
-            pieceArray[i] = '';
-        };
-    };
-
-    return {
-        setBoard,
-        getBoard,
-        reset,
-    }
+    
 })();
+
+const displayController = (() => {
+
+})();
+
+const Player = () => {
+    
+}
 
 const gameFlow = () => {
 
