a782787a945c8fbf077137e910bb095a5f647c4f
katynightshade <98486819+katynightshade@users.noreply.github.com>
2022-05-19 12:41:49 -0600

    gameFlow.winner + activePlayer - incomplete

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //array is not populating on click
-const makeGameboard = (() => {
-    let gameboard = new Array();
+const makeBoard = (() => {
+    let board = new Array();
     let gameArray = document.querySelectorAll('.game-array');
 
     for (let i = 0; i < gameArray.length; i++) {
-        gameboard.push(gameArray[i].textContent);
+        board.push(gameArray[i].textContent);
     }
 
     return {
-        gameboard,
+        board,
         gameArray,
     }
 })();
@@ -23,17 +23,17 @@ const displayController = (() => {
     const gameArray = document.querySelectorAll('.game-array').forEach((item) => {
         item.addEventListener('click', function placeItems(e) {
             if (item.textContent !== '') return;
-            makeGameboard.gameboard[e.target.id] = selection;
+            makeBoard.board[e.target.id] = selection;
             item.textContent = selection;
             selection = nextTurn[selection];
-            if (gameFlow.winner = true) {
+            if (gameFlow.winner === true) {
                 item.removeEventListener('click', placeItems());
             }
             gameFlow.round += 1;
             gameFlow.winnerCheck();
             if (gameFlow.winner === false) {
                 if (gameFlow.round < 9) {
-                    gameFlow.checkPlayer();
+                    gameFlow.nextPlayer;
                 } else if (gameFlow.round === 9) {
                     gameFlow.declareTie();
                 };
@@ -52,64 +52,58 @@ const displayController = (() => {
 })();
 
 const Player = (sign) => {
-    this.sign = sign;
-
-    const getSign = () => {
-        return sign;
-    }
-
     return {
-        getSign,
+        sign,
     }
 }
 
 const gameFlow = (() => {
     let playerX = Player('X');
     let playerO = Player('O');
-
-    let activePlayer = playerX;
     let round = 0;
     let winner = false;
+    let activePlayer = playerX;
+    let playerTurn = {
+        playerX : playerO,
+        playerO : playerX,
+    }
 
     const winnerMsg = document.getElementById('winner-msg');
     const playerMsg = document.getElementById('player-msg');
 
-    const winningCombos = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ];
-
     function winnerCheck() {
-        winningCombos.forEach((item, index) => {
-            if (makeGameboard.gameboard[item[0]] === this.activePlayer.getSign && makeGameboard.gameboard[item[1]] === this.activePlayer.getSign && makeGameboard.gameboard[item[2]] === this.activePlayer.getSign) {
-                winnerMsg.textContent = `Player ${activePlayer.getSign} wins!`;
-                playerMsg.style.display = 'none';
-                this.winner = true;
-            }
-        });
+        let board = makeBoard.board;
+        if (
+            (board[0] === board[1] && board[1] === board[2] && board[0] === board[2] && board[0] !== '' && board[1] !== '' && board[2] !== '') ||
+            (board[3] === board[4] && board[4] === board[5] && board[3] === board[5] && board[3] !== '' && board[4] !== '' && board[5] !== '') ||
+            (board[6] === board[7] && board[7] === board[8] && board[6] === board[8] && board[6] !== '' && board[7] !== '' && board[8] !== '') ||
+            (board[0] === board[3] && board[3] === board[6] && board[0] === board[6] && board[0] !== '' && board[3] !== '' && board[6] !== '') ||
+            (board[1] === board[4] && board[4] === board[7] && board[1] === board[7] && board[1] !== '' && board[4] !== '' && board[7] !== '') ||
+            (board[2] === board[5] && board[5] === board[8] && board[2] === board[8] && board[2] !== '' && board[5] !== '' && board[8] !== '') ||
+            (board[0] === board[4] && board[4] === board[8] && board[0] === board[8] && board[0] !== '' && board[4] !== '' && board[8] !== '') ||
+            (board[2] === board[4] && board[4] === board[6] && board[2] === board[6] && board[2] !== '' && board[4] !== '' && board[6] !== '')
+        ) {
+            winnerMsg.textContent = `Player ${activePlayer.sign} wins!`;
+            playerMsg.style.display = 'none';
+            winner = true;
+        }
     }
 
-    function checkPlayer() {
-        this.activePlayer === playerX ? this.activePlayer = playerO : this.activePlayer = playerX;
-        console.log('active player:' + activePlayer.getSign);
+    function nextPlayer() {
+        activePlayer = playerTurn[activePlayer];
     }
 
     function declareTie() {
         winnerMsg.textContent = 'Tied game!';
+        playerMsg.style.display = 'none';
     }
 
     return {
-        activePlayer,
         round,
         winner,
+        activePlayer,
+        nextPlayer,
         winnerCheck, 
-        checkPlayer,
         declareTie,
     }
 })();
